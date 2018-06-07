1a50e47f8c23cf1b2c70759b7bcb94a2c8fa39ef
James Eddy <james.a.eddy@gmail.com>
2018-06-07 01:47:33 -0700

    allow for variable number of groups per user

--- util/add_users.sh
+++ util/add_users.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # bash /path/to/file.csv
+function join_by { local IFS="$1"; shift; echo "$*"; }
 
 while read line; do
   a=($(echo "$line" | tr ',' '\n'))
   b=($(echo "${a[2]}" | tr ';' '\n'))
   # add the user
   useradd -m -p $(openssl passwd -crypt "${a[1]}") -s /bin/bash "${a[0]}"
-  usermod -a -G "${b[0]},${b[1]}" "${a[0]}"
+  usermod -a -G $(join_by , "${b[@]}") "${a[0]}"
 done < "$1"
