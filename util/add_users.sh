388d5e5ebf0dfbdd2289b50d416af80b5447c075
James Eddy <james.a.eddy@gmail.com>
2018-06-07 15:12:43 -0700

    check whether user already exists in add_users

--- util/add_users.sh
+++ util/add_users.sh
@@ -6,7 +6,12 @@ function join_by { local IFS="$1"; shift; echo "$*"; }
 while read line; do
   a=($(echo "$line" | tr ',' '\n'))
   b=($(echo "${a[2]}" | tr ';' '\n'))
-  # add the user
-  useradd -m -p $(openssl passwd -crypt "${a[1]}") -s /bin/bash "${a[0]}"
-  usermod -a -G $(join_by , "${b[@]}") "${a[0]}"
+  getent passwd "${a[0]}" > /dev/null
+  if [ $? -eq 0 ]; then
+    # add the user
+    useradd -m -p $(openssl passwd -crypt "${a[1]}") -s /bin/bash "${a[0]}"
+    usermod -a -G $(join_by , "${b[@]}") "${a[0]}"
+  else
+    echo "User ${a[0]} already exists; skipping."
+  fi
 done < "$1"
